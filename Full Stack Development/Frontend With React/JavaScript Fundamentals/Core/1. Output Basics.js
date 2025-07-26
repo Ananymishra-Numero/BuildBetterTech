86150039ba10aeb77b8ad0618b419f7e824f4489
Anany Mishra <anany.mishra.real@gmail.com>
2025-07-26 07:45:38 +0000

    Done

---
+++ Full Stack Development/Frontend With React/JavaScript Fundamentals/Core/1. Output Basics.js
@@ -0,0 +1,28 @@
+// Outputting text to the console
+console.log("Hello, World!");
+
+// Single line comment
+
+/*
+Multi-line 
+comment
+
+console.log(600);
+The above line won't output anything, since it's within a comment
+*/
+
+console.log(42); // prints 42
+console.log("Learning is fun!"); // prints "Learning is fun!"
+console.log(true); // prints true
+
+console.log(2 + 2); // Outputs: 4
+console.log("2 + 2"); // Outputs: 2 + 2
+console.log("Hello, " + "World!"); // Outputs: Hello, World!
+console.log("2" + "2"); // Outputs: 22, because + operation applied on strings concatenates them
+console.log(2 + 2 > 5); // Outputs: false, because 2 + 2 is not greater than 5.
+
+console.log(5 < 6);   // true
+console.log(5 > 6);   // false
+console.log(5 <= 5);  // true
+console.log(5 >= 10); // false
+
